28efbc1d2fca8e947495d5be5d5efab5dbec3b3d
Wilian Brito Pereira <wilianbps@hotmail.com>
2021-09-22 14:30:09 -0300

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,120 @@
+/* abre e fecha o menu quando clicar no ícone: hambuerguer e X*/
+
+const nav = document.querySelector('#header nav')
+const toggle = document.querySelectorAll('nav .toggle')
+
+for (const element of toggle) {
+  element.addEventListener('click', function () {
+    nav.classList.toggle('show')
+  })
+}
+
+/* abre e fecha o menu quando clicar no ícone: hambuerguer e X*/
+const links = document.querySelectorAll('nav ul li a')
+
+for (const link of links) {
+  link.addEventListener('click', function () {
+    nav.classList.remove('show')
+  })
+}
+
+/* mudar o header da página quando der o scroll*/
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight
+
+function changeHeaderWhenScroll() {
+  if (window.scrollY >= navHeight) {
+    header.classList.add('scroll')
+  } else {
+    header.classList.remove('scroll')
+  }
+}
+
+/* SWIPER*/
+
+const swiper = new Swiper('.swiper', {
+  slidesPerView: 1,
+  pagination: {
+    el: '.swiper-pagination'
+  },
+  mousewheel: true,
+  keyboard: true,
+  breakpoints: {
+    767: {
+      slidesPerView: 2,
+      setWrapperSize: true
+    },
+    1300: {
+      slidesPerView: 3,
+      setWrapperSize: true
+    }
+  }
+})
+
+/* scrollrevealjs: mostrar elementos quando dar scroll na página */
+
+const scrollReveal = ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700,
+  reset: true
+})
+
+scrollReveal.reveal(
+  `#home .image, #home .text, #about .image, #about .text, #about .company_numbers, #services header, #services .card, #clients header, #clients .clients,#team header, #team .card, #contact .text, #contact .links, footer .brand, footer .social`,
+  { interval: 100 }
+)
+
+/* Botão voltar para o topo */
+
+/* window.scrollY >= 400 && window.scrollY <= 4900  */
+
+const backToTopButton = document.querySelector('.back-to-top')
+
+function backToTop() {
+  if (window.scrollY >= 300 && window.scrollY <= 4900) {
+    backToTopButton.classList.add('show')
+  } else {
+    backToTopButton.classList.remove('show')
+  }
+}
+
+/* Menu ativo conforme a seção visível na página */
+const sections = document.querySelectorAll('main section[id]')
+function activateMenuAtCurrentSection() {
+  const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4
+
+  for (const section of sections) {
+    const sectionTop = section.offsetTop
+    const sectionHeight = section.offsetHeight
+    const sectionId = section.getAttribute('id')
+
+    const checkpointStart = checkpoint >= sectionTop
+    const checkpointEnd = checkpoint <= sectionTop + sectionHeight
+
+    if (checkpointStart && checkpointEnd) {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.add('active')
+    } else {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.remove('active')
+    }
+  }
+}
+
+window.addEventListener('scroll', function () {
+  changeHeaderWhenScroll()
+  backToTop()
+  activateMenuAtCurrentSection()
+})
+
+/* typed */
+var typed = new Typed('.typed', {
+  strings: ['Residencial, Comercial e Condomínios'],
+  typeSpeed: 50,
+  backSpeed: 50,
+  loop: true,
+  showCursor: false
+})
